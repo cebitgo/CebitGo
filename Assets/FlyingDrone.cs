bced6193b7162454fa9ffb0f8c6e4b0b818dfb30
Bryan Hempen <brhempen@gmail.com>
2017-11-25 17:30:04 +0100

    shoot objects

---
+++ Assets/FlyingDrone.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingDrone : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 newPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z - 0.5f * Time.deltaTime);
+		transform.position = newPosition;
+	}
+
+	void OnCollisionEnter()
+	{
+		Destroy(this.gameObject);
+	}
+
+
+}
---
+++ Assets/ShootBullet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootBullet : MonoBehaviour {
+
+	public GameObject bulletPrefab;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown("space"))
+		{
+			GameObject bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
+
+			// Add velocity to the bullet
+			bullet.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 2) * 6;
+			Destroy(bullet, 10.0f);
+		}
+            
+	}
+}
