09f519977c84cfebe2dc3939feeb74cfd11ba496
Tobias Pott <mailme@tobiaspott.de>
2017-11-26 00:53:50 +0100

    Corrected logo size Fixed POIMarker prefab

--- Assets/UI/Scripts/CameraFacingMarker.cs
+++ Assets/UI/Scripts/CameraFacingMarker.cs
@@ -1,27 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CameraFacingMarker : MonoBehaviour {
+public class CameraFacingMarker : MonoBehaviour
+{
 
-	private Camera _thisCamera;
-	public float scaleMin = 0.1f;
-	public float scaleMax = 5;
-	public float scaleFactor = 1;
+    private Camera _thisCamera;
+    public float scaleMin = 0.1f;
+    public float scaleMax = 5;
+    public float scaleFactor = 1;
 
 
-	// Use this for initialization
-	void Awake () {
+    // Use this for initialization
+    void Awake()
+    {
         _thisCamera = Camera.main;
-	}
-	
-	void LateUpdate()
-	{
-		transform.LookAt(transform.position + _thisCamera.transform.rotation * Vector3.forward,
-                         _thisCamera.transform.rotation * Vector3.up);
+    }
 
-		//float tempScale = scaleMin + ((1 - (Mathf.Clamp(Vector3.Distance(_thisCamera.transform.position, transform.position), 0, 1250) / 1250)) * (scaleMax - scaleMin));
-		//transform.localScale = new Vector3(tempScale, tempScale, 1);
-	}
+    void LateUpdate()
+    {
+        //transform.LookAt(transform.position + _thisCamera.transform.rotation * Vector3.forward,
+        //                 _thisCamera.transform.rotation * Vector3.up);
+        transform.rotation = Quaternion.LookRotation(this.transform.position - _thisCamera.transform.position, Vector3.up);
+
+
+        //float tempScale = scaleMin + ((1 - (Mathf.Clamp(Vector3.Distance(_thisCamera.transform.position, transform.position), 0, 1250) / 1250)) * (scaleMax - scaleMin));
+        //transform.localScale = new Vector3(tempScale, tempScale, 1);
+    }
 }
 
 
